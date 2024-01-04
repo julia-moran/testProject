ca1e3b4fadff974bf1cb812aa2ed646d05ea4b85
julia-moran <juliamoran1001@gmail.com>
2024-01-04 12:50:54 -0500

    Display choices

--- App.js
+++ App.js
@@ -13,23 +13,41 @@ export default function() {
   const db = SQLite.openDatabase('test.db');
   const [isLoading, setIsLoading] = useState(true);
   const [scenes, setScenes] = useState([]);
+  const [choices, setChoices] = useState([]);
   const [currentSceneText, setCurrentSceneText] = useState("");
   const [currentNextSceneID, setCurrentNextSceneID] = useState("");
+  const [currentChoiceSceneID, setCurrentChoiceSceneID] = useState("");
+  const [currentChoiceText, setCurrentChoiceText] = useState("");
+  const [currentChoiceLeadsToID, setCurrentChoiceLeadsToID] = useState("");
   const [sceneTextInRun, setSceneTextInRun] = useState("");
   const [nextSceneInRun, setNextSceneInRun] = useState("");
+  const [sceneIDInRun, setSceneIDInRun] = useState("");
+  const [choicesInRun, setChoicesInRun] = useState([]);
 
   useEffect(() => {
     db.transaction(tx => {
-      tx.executeSql('CREATE TABLE IF NOT EXISTS scene (id INTEGER PRIMARY KEY AUTOINCREMENT, scene_text TEXT, next_scene_id INTEGER)')
+      tx.executeSql('CREATE TABLE IF NOT EXISTS scene4 (id INTEGER PRIMARY KEY AUTOINCREMENT, scene_text TEXT, next_scene_id INTEGER)')
     });
 
     db.transaction(tx => {
-      tx.executeSql('SELECT * FROM scene', null,
+      tx.executeSql('CREATE TABLE IF NOT EXISTS choice (choice_id INTEGER PRIMARY KEY AUTOINCREMENT, scene_id INTEGER REFERENCES scene4(id), choice_text TEXT, next_scene_id INTEGER)')
+    });
+
+    db.transaction(tx => {
+      tx.executeSql('SELECT * FROM scene4', null,
         (txObj, resultSet) => setScenes(resultSet.rows._array),
         (txObj, error) => console.log(error)
       );
     });
 
+    db.transaction(tx => {
+      tx.executeSql('SELECT * FROM choice', null,
+        (txObj, resultSet) => setChoices(resultSet.rows._array),
+        (txObj, error) => console.log(error)
+      );
+    });
+
+    
     setIsLoading(false);
   }, []);
 
@@ -43,13 +61,45 @@ export default function() {
 
   const addScene = () => {
     db.transaction(tx => {
-      tx.executeSql('INSERT INTO scene (scene_text, next_scene_id) values (?, ?)', [currentSceneText, currentNextSceneID],
+      tx.executeSql('INSERT INTO scene4 (scene_text, next_scene_id) values (?, ?)', [currentSceneText, currentNextSceneID],
         (txObj, resultSet) => {
           let existingScenes = [...scenes];
           existingScenes.push({ id: resultSet.insertId, scene_text: currentSceneText, next_scene_id: currentNextSceneID});
           setScenes(existingScenes);
           setCurrentSceneText("");
           setCurrentNextSceneID("");
+        },
+        (txObj, error) => console.log(error)
+      );
+    });
+  }
+
+  const addChoice = () => {
+    db.transaction(tx => {
+      tx.executeSql('INSERT INTO choice (scene_id, choice_text, next_scene_id) values (?, ?, ?)', [currentChoiceSceneID, currentChoiceText, currentChoiceLeadsToID],
+        (txObj, resultChoices) => {
+          let existingChoices = [...choices];
+          existingChoices.push({ id: resultChoices.insertId, scene_id: currentChoiceSceneID, choice_text: currentChoiceText, next_scene_id: currentChoiceLeadsToID});
+          setChoices(existingChoices);
+          setCurrentChoiceText("");
+          setCurrentChoiceLeadsToID("");
+          setCurrentChoiceSceneID("");
+        },
+        (txObj, error) => console.log(error)
+      );
+    });
+  }
+    
+
+
+  const getChoices = (sceneID) => {
+    console.log(sceneID);
+    db.transaction(tx => {
+      tx.executeSql('SELECT * FROM choice WHERE scene_id = ?', [sceneID],
+        (txObj, resultChoices) => {
+          setChoicesInRun(resultChoices.rows._array);
+          //console.log(choicesInRun);
+          //setChoicesInRun([]);
         },
         (txObj, error) => console.log(error)
       );
@@ -67,27 +117,63 @@ export default function() {
     });
   };
 
+    const showChoices = () => {
+    return choices.map((choice, index) => {
+      return (
+        <View key={index} style={styles.row}>
+          <Text>{choice.choice_text}</Text>
+          <Text>{choice.scene_id}</Text>
+          <Text>{choice.next_scene_id}</Text>
+        </View>
+      );
+    });
+  };
+
+  const showChoiceButtons = () => {
+    console.log(choicesInRun);
+    return choicesInRun.map((choiceInRun, index) => {
+      return (
+        <View key={index} style={styles.row}>
+          <Button title={choiceInRun.choice_text}/>
+        </View>
+      );
+    });
+  };
+
   const showSceneByID = () => {
     setSceneTextInRun(scenes.at(0).scene_text);
     setNextSceneInRun(scenes.at(0).next_scene_id);
-  }
+    setSceneIDInRun(scenes.at(0).id);
+    //getChoices();
+  };
 
   const getNextScene = () => {
     let nextSceneID = (parseInt(nextSceneInRun)) - 1;
     setSceneTextInRun(scenes.at(nextSceneID).scene_text);
     setNextSceneInRun(scenes.at(nextSceneID).next_scene_id);
-  }
+    setSceneIDInRun(scenes.at(nextSceneID).id);
+    getChoices(scenes.at(nextSceneID).id);
+  };
 
   return (
     <View style={styles.container}>
       <TextInput value={currentSceneText} placeholder='Scene Text' onChangeText={setCurrentSceneText}/>
       <TextInput value={currentNextSceneID} placeholder='Next Scene ID' onChangeText={setCurrentNextSceneID}/>
       <Button title='Add Scene' onPress={addScene}/>
+
+      <TextInput value={currentChoiceSceneID} placeholder='Choice For Scene' onChangeText={setCurrentChoiceSceneID}/>
+      <TextInput value={currentChoiceText} placeholder='Choice Text' onChangeText={setCurrentChoiceText}/>
+      <TextInput value={currentChoiceLeadsToID} placeholder='Choice Leads to Scene ID' onChangeText={setCurrentChoiceLeadsToID}/>
+      <Button title='Add Choice' onPress={addChoice}/>
       {showScenes()}
+      {showChoices()}
       <Button title='Start Story' onPress={showSceneByID}/>
       <Text>{sceneTextInRun}</Text>
       <Text>{nextSceneInRun}</Text>
+      <Text>{sceneIDInRun}</Text>
+      {showChoiceButtons()}
       <Button title='Next Scene' onPress={getNextScene}/>
+      
       <StatusBar style="auto" />
     </View>
   );
